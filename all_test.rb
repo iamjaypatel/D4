bfdbd4f7f2d8e43c99655c1550d1698d244b1169
Jay Patel <jay26597@gmail.com>
2019-04-11 15:16:22 -0400

    verifier test and simplecov file

---
+++ all_test.rb
@@ -0,0 +1,4 @@
+require 'simplecov'
+Simplecov.start
+
+require_relative 'verifier_test'
---
+++ verifier_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require_relative 'verifier'
+
+# Test Class for verifier.rb file
+class VerifierTest < Minitest::Test
+
+end
