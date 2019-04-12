6fc187ac5b5c62968c4b3491025e5d39f42b6e96
Kevin Wang <kew122@pitt.edu>
2019-04-12 19:14:47 -0400

    Optimized w/ memoization and fixed most Rubocop errors

--- verifier.rb
+++ verifier.rb
@@ -2,141 +2,146 @@
 
 require 'flamegraph'
 
-Flamegraph.generate('./initial.html') do
-  # Globals
-  $prev_hash = '0' # Keeps track of previous hash
-  $prev_time = '0.0' # Keeps track of previous timestamp
-  $line_num = 0 # Keeps track of line number
-  $addresses = {} # Hash of {addresses => billcoins}
+# Checks if block number is valid
+def check_num(block_num, line_num)
+  return if block_num.to_i == line_num
 
-  # Checks if block number is valid
-  def check_num(block_num)
-    if block_num.to_i != $line_num
-      abort("Line #{$line_num}: Invalid block number #{block_num}, should be #{$line_num}\nBLOCKCHAIN INVALID")
-    end
+  abort("Line #{line_num}: Invalid block number #{block_num}, should be #{line_num}\nBLOCKCHAIN INVALID")
+end
+
+# Checks if stored previous hash matches the previous hash in the block
+def check_prev_hash(prev_hash, block_prev_hash, hash, line_num)
+  if prev_hash != block_prev_hash
+    abort("Line #{line_num}: Previous hash was #{block_prev_hash}, should be #{prev_hash}\nBLOCKCHAIN INVALID")
   end
+  hash.delete("\n")
+end
 
-  # Checks if stored previous hash matches the previous hash in the block
-  def check_prev_hash(prev_hash, hash)
-    if $prev_hash != prev_hash
-      abort("Line #{$line_num}: Previous hash was #{prev_hash}, should be #{$prev_hash}\nBLOCKCHAIN INVALID")
-    else
-      $prev_hash = hash.delete("\n")
-    end
+# Checks that timestamp of current block is not greater than or equal to the block timestamp
+def check_time(time, prev_time, line_num)
+  prev_time_sec = prev_time.split('.')[0].to_i
+  prev_time_nano = prev_time.split('.')[1].to_i
+  curr_time_sec = time.split('.')[0].to_i
+  curr_time_nano = time.split('.')[1].to_i
+  if prev_time_sec >= curr_time_sec && prev_time_nano >= curr_time_nano
+    abort("Line #{line_num}: Previous timestamp #{prev_time} >= new timestamp #{time}\nBLOCKCHAIN INVALID")
   end
+  time
+end
 
-  # Checks that timestamp of current block is not greater than or equal to the block timestamp
-  def check_time(time)
-    prev_time_sec = $prev_time.split('.')[0].to_i
-    prev_time_nano = $prev_time.split('.')[1].to_i
-    curr_time_sec = time.split('.')[0].to_i
-    curr_time_nano = time.split('.')[1].to_i
-    if prev_time_sec >= curr_time_sec && prev_time_nano >= curr_time_nano
-      abort("Line #{$line_num}: Previous timestamp #{$prev_time} >= new timestamp #{time}\nBLOCKCHAIN INVALID")
-    end
-    $prev_time = time
+# Checks that addresses are valid
+def check_addresses(addresses, line_num)
+  if addresses[0].length > 6 || addresses[0].match?(/[A-Za-z]/) && addresses[0] != 'SYSTEM'
+    abort("Line #{line_num}: Invalid address #{addresses[0]}\nBLOCKCHAIN INVALID")
+  elsif addresses[1].length > 6 || addresses[1].match?(/[A-Za-z]/)
+    abort("Line #{line_num}: Invalid address #{addresses[1]}\nBLOCKCHAIN INVALID")
   end
+end
 
-  # Checks that addresses are valid
-  def check_addresses(addresses)
-    if addresses[0].length > 6
-      abort("Line #{$line_num}: Invalid address #{addresses[0]}\nBLOCKCHAIN INVALID")
-    elsif addresses[1].length > 6
-      abort("Line #{$line_num}: Invalid address #{addresses[1]}\nBLOCKCHAIN INVALID")
-    end
+def check_format(transactions, line_num)
+  return if transactions.include?('>')
+
+  abort("Line #{line_num}: Could not parse transactions list '#{transactions}'\nBLOCKCHAIN INVALID")
+end
+
+# Obtains the billcoins and addresses involved in each transaction, then executes each transaction
+# Returns an array of the addresses involved in transactions in the block
+def process_transactions(transactions, line_num, addresses)
+  block_addresses = []
+  transactions.split(':').each do |transaction|
+    billcoins = transaction[/(?<=\()\w+(?=\)$)/].to_i # Regex to obtain billcoins traded in parentheses
+    address_pair = transaction.gsub(/\(.*?\)/, '').split('>') # Obtain pair of addresses involved in transaction
+    check_addresses(address_pair, line_num)
+    block_addresses.push(address_pair[0], address_pair[1])
+    withdraw(billcoins, address_pair[0], addresses) if address_pair[0] != 'SYSTEM'
+    add(billcoins, address_pair[1], addresses)
   end
+  block_addresses
+end
 
-  def check_format(transactions)
-    unless transactions.include?('>')
-      abort "Line #{$line_num}: Could not parse transactions list '#{transactions}'\nBLOCKCHAIN INVALID"
-    end
-    # Other possible checks?
+# Subtracts billcoins from the sender
+def withdraw(billcoins, sender, addresses)
+  if addresses[sender].nil?
+    addresses[sender] = -billcoins
+  else
+    addresses[sender] -= billcoins
   end
+end
 
-  # Obtains the billcoins and addresses involved in each transaction, then executes each transaction
-  # Returns an array of the addresses involved in transactions in the block
-  def process_transactions(transactions)
-    block_addresses = []
-    transactions.split(':').each do |transaction|
-      billcoins = transaction[/\(.*?\)/].gsub(/[()]/, '').to_i # Regex to obtain billcoins traded in parentheses
-      addresses = transaction.gsub(/\(.*?\)/, '').split('>') # Obtain pair of addresses involved in transaction
-      check_addresses(addresses)
-      block_addresses.push(addresses[0], addresses[1])
-      withdraw(billcoins, addresses[0]) if addresses[0] != 'SYSTEM'
-      add(billcoins, addresses[1])
-    end
-    block_addresses
+# Adds billcoins to the receiver
+def add(billcoins, receiver, addresses)
+  if addresses[receiver].nil?
+    addresses[receiver] = billcoins
+  else
+    addresses[receiver] += billcoins
   end
+end
 
-  # Subtracts billcoins from the sender
-  def withdraw(billcoins, sender)
-    if $addresses[sender].nil?
-      $addresses[sender] = -billcoins
-    else
-      $addresses[sender] -= billcoins
+# Calculates the hash for the current block
+# Memoization?
+def calc_hash(line, calculations)
+  string_to_hash = "#{line[0]}|#{line[1]}|#{line[2]}|#{line[3]}".unpack('U*')
+  sum = 0
+  string_to_hash.each do |x|
+    if calculations[x].nil?
+      calculations[x] = ((x**3000) + (x**x) - (3**x)) * (7**x)
     end
+    sum += calculations[x]
   end
+  result = (sum % 65_536).to_s(16)
+  result
+end
 
-  # Adds billcoins to the receiver
-  def add(billcoins, receiver)
-    if $addresses[receiver].nil?
-      $addresses[receiver] = billcoins
-    else
-      $addresses[receiver] += billcoins
-    end
-  end
+def check_hash(found_hash, line, line_num)
+  return if found_hash == line[4].delete("\n")
 
-  # Calculates the hash for the current block
-  def calc_hash(line)
-    string_to_hash = "#{line[0]}|#{line[1]}|#{line[2]}|#{line[3]}".unpack('U*')
-    sum = 0
-    string_to_hash.each do |x|
-      sum += ((x**3000) + (x**x) - (3**x)) * (7**x)
-    end
-    result = (sum % 65_536).to_s(16)
-    result
-  end
+  puts "Line #{line_num}: String '#{line[0]}|#{line[1]}|#{line[2]}|#{line[3]}' hash set to #{line[4]}, should be #{found_hash}"
+  puts 'BLOCKCHAIN INVALID'
+  exit 1
+end
 
-  def check_hash(found_hash, line)
-    if found_hash != line[4].delete("\n")
-      abort("Line #{$line_num}: String '#{line[0]}|#{line[1]}|#{line[2]}|#{line[3]}' hash set to #{line[4]}, should be #{found_hash}\nBLOCKCHAIN INVALID")
-    end
-  end
+# Reads through the file and extracts the information from each line
+# Checks for any invalidity, then prints out the addresses and billcoins
+def read(file)
+  line_num = 0
+  prev_hash = '0'
+  prev_time = '0.0'
+  addresses = {}
+  calculations = {}
+  File.foreach(file) do |line|
+    line = line.split('|') # Split the line using the pipes
+    block_num = line[0] # Block number
+    block_prev_hash = line[1] # Hash of previous block
+    transactions = line[2] # Sequence of transactions
+    timestamp = line[3] # Timestamp
+    hash = line[4] # Hash of the first four elements
+    found_hash = calc_hash(line, calculations)
 
-  # Reads through the file and extracts the information from each line
-  # Checks for any invalidity, then prints out the addresses and billcoins
-  def read(file)
-    File.foreach(file).with_index do |line, line_num|
-      line = line.split('|') # Split the line using the pipes
-      $line_num = line_num
-      block_num = line[0] # Block number
-      prev_hash = line[1] # Hash of previous block
-      transactions = line[2] # Sequence of transactions
-      timestamp = line[3] # Timestamp
-      hash = line[4] # Hash of the first four elements
-      found_hash = calc_hash(line)
+    check_num(block_num, line_num)
+    check_format(transactions, line_num)
+    check_hash(found_hash, line, line_num)
+    prev_hash = check_prev_hash(prev_hash, block_prev_hash, hash, line_num)
+    prev_time = check_time(timestamp, prev_time, line_num)
 
-      check_format(transactions)
-      check_num(block_num)
-      check_hash(found_hash, line)
-      check_prev_hash(prev_hash, hash)
-      check_time(timestamp)
-
-      addresses = process_transactions(transactions).uniq # Get all unique addresses involved in transactions in the block
-      addresses.each do |address|
-        if address != 'SYSTEM' && $addresses[address] < 0 # If an address has negative billcoins
-          abort("Line #{$line_num}: Invalid block, address #{address} has #{$addresses[address]} billcoins!\nBLOCKCHAIN INVALID")
-        end
+    uniq_addresses = process_transactions(transactions, line_num, addresses).uniq # Get unique addresses w/ transactions
+    uniq_addresses.each do |address|
+      if address != 'SYSTEM' && (addresses[address]).negative? # If an address has negative billcoins
+        puts "Line #{line_num}: Invalid block, address #{address} has #{addresses[address]} billcoins!"
+        puts 'BLOCKCHAIN INVALID'
+        exit 1
       end
     end
-    $addresses = $addresses.delete_if { |_, billcoins| billcoins == 0 } # Remove all addresses with 0 billcoins
-    $addresses.sort.to_h.each do |address, billcoins|
-      puts "#{address}: #{billcoins} billcoins" # Print out addresses and billcoins
-    end
+    line_num += 1
   end
+  addresses = addresses.delete_if { |_, billcoins| billcoins.zero? } # Remove all addresses with 0 billcoin
+  addresses.sort.each do |data|
+    puts "#{data[0]}: #{data[1]} billcoins" # Print out addresses and billcoins
+  end
+end
 
+Flamegraph.generate('./initial.html') do
   if ARGV.empty? # If no command line args
-    puts("Usage: ruby verifier.rb <name_of_file>\n\tname_of_file = name of file to verify")
+    puts "Usage: ruby verifier.rb <name_of_file>\n\tname_of_file = name of file to verify"
     exit 1
   else
     file = ARGV[0]
